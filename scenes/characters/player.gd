f6749bff5936d3303864cd029957aadfcd22b08c
Kaan Salı <kaan_sali@hotmail.com>
2022-11-04 20:11:22 +0300

    Made Player movement system smoother.

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -8,10 +8,13 @@ class_name Player
 @export var max_speed := 400.0
 @export var gravity_scale := 1.0
 @export var fall_scale := 2.0
+@export var deaccelerate_time := 0.2 
 @export_group("Jump Variables")
 @export var jump_speed := 400.0
 
 @onready var collision : CollisionShape2D = $CollisionShape2D
+
+var is_stopped := true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -32,11 +35,14 @@ func process_non_editor(delta:float):
 
 func set_movement_vector(delta:float)->void:
 	var horizontal_direction : float = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
-	velocity.x += horizontal_direction * max_speed/6.0
+	velocity.x += horizontal_direction * max_speed / 6.0
 	if(abs(velocity.x)>max_speed):
 		velocity = Vector2(signf(velocity.x)*max_speed,velocity.y)
-	if(horizontal_direction == 0):
-		velocity = Vector2(0.0,velocity.y)
+	if(horizontal_direction == 0 and abs(velocity.x) > 0 and is_stopped != false):
+		create_tween().tween_property(self,"velocity:x",0,deaccelerate_time).connect("finished",tween_stopped)
+		is_stopped = false
+		#velocity = Vector2(0,velocity.y)
+	# Map horizontal Limits
 	if(global_position.x <= 32.0 and velocity.x < 0.0):
 		velocity.x = 0
 	
@@ -46,16 +52,21 @@ func set_jump()->void:
 
 func apply_gravity()->void:
 	var scale = 1.0
+	# Fall Gravity
 	if((Input.get_action_strength("jump") == 0.0 and velocity.y < 0.0) or velocity.y > 0.0):
 		scale = fall_scale
 	else:
 		scale = gravity_scale 
 	velocity -= jump_speed/18.0 * scale * Vector2.UP
 
+func tween_stopped()->void:
+	is_stopped=true
+
 func set_character_texture(value):
 	if Engine.is_editor_hint():
 		character_texture = value
 		$character_texture.set_texture(value)
+
 func set_character_spawn_point(node:Node2D):
 	spawn_point = node
 	if(is_instance_valid(node)):
